7378c115a610d22b3c6e528ef21cc0481651eac9
Administrator <851822132@qq.com>
2019-03-24 21:25:28 +0800

    技术整合-同一配置

--- config-client/src/main/java/com/ysh/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/ysh/configclient/ConfigClientApplication.java
@@ -3,11 +3,16 @@ package com.ysh.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+//开启服务注册的功能
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
     @Value("${foo}")
     private String foo;
--- config-server/src/main/java/com/ysh/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ysh/configserver/ConfigServerApplication.java
@@ -3,9 +3,13 @@ package com.ysh.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+//开启服务注册的功能
+@EnableEurekaClient
+//@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
