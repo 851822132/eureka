05da3d1e849b74084f69579e2259437b00e0b914
ysh <851822132@qq.com>
2019-03-29 17:08:10 +0800

    添加springbootadmin监测，springcloud链路追踪

--- zipkin-server/src/main/java/com/ysh/zinpinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/ysh/zinpinserver/ZipkinServerApplication.java
@@ -2,7 +2,10 @@ package com.ysh.zinpinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import zipkin2.server.internal.EnableZipkinServer;
 import zipkin.server.internal.EnableZipkinServer;
 
 /**
@@ -16,5 +19,6 @@ public class ZipkinServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
     }
+
 
 }
